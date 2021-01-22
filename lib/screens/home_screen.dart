dad158afad75e2e6a07b644fd0aa97b6a78101cb
Jann <jann.peters@web.de>
2021-01-22 15:34:39 +0100

    styled datepicker with parent theme to get color and font todo: consider making the list of day rows scrollable to show even more and allow the user to interact - NO TO MUCH SWIPING ACTION

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -200,163 +200,166 @@ class _MyHomePageState extends State<MyHomePage> {
           automaticallyImplyLeading: false,
         ),
       ),
-      body: Builder(
-        builder: (context) => TabBarView(
-          children: [
-            SafeArea(
-              child: Stack(
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 5),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            DayRow(
-                                day: days[0],
-                                date: dateStampToday,
-                                dataMap: dataMap[dateStampToday],
-                                isToday: true),
-                            DayRow(
-                                day: days[1],
-                                date:
-                                    dateStampToday.subtract(Duration(days: 1)),
-                                dataMap: dataMap[dateStampToday
-                                        .subtract(Duration(days: 1))] ??
-                                    {
-                                      'Breakfast': '',
-                                      'Lunch': '',
-                                      'Dinner': ''
-                                    }),
-                            DayRow(
-                                day: days[2],
-                                date:
-                                    dateStampToday.subtract(Duration(days: 2)),
-                                dataMap: dataMap[dateStampToday
-                                        .subtract(Duration(days: 2))] ??
-                                    {
-                                      'Breakfast': '',
-                                      'Lunch': '',
-                                      'Dinner': ''
-                                    }),
-                            DayRow(
-                                day: days[3],
-                                date:
-                                    dateStampToday.subtract(Duration(days: 3)),
-                                dataMap: dataMap[dateStampToday
-                                        .subtract(Duration(days: 3))] ??
-                                    {
-                                      'Breakfast': '',
-                                      'Lunch': '',
-                                      'Dinner': ''
-                                    }),
-                            DayRow(
-                                day: days[4],
-                                date:
-                                    dateStampToday.subtract(Duration(days: 4)),
-                                dataMap: dataMap[dateStampToday
-                                        .subtract(Duration(days: 4))] ??
-                                    {
-                                      'Breakfast': '',
-                                      'Lunch': '',
-                                      'Dinner': ''
-                                    }),
-                            DayRow(
-                                day: days[5],
-                                date:
-                                    dateStampToday.subtract(Duration(days: 5)),
-                                dataMap: dataMap[dateStampToday
-                                        .subtract(Duration(days: 5))] ??
-                                    {
-                                      'Breakfast': '',
-                                      'Lunch': '',
-                                      'Dinner': ''
-                                    }),
-                            DayRow(
-                                day: days[6],
-                                date:
-                                    dateStampToday.subtract(Duration(days: 6)),
-                                dataMap: dataMap[dateStampToday
-                                        .subtract(Duration(days: 6))] ??
-                                    {
-                                      'Breakfast': '',
-                                      'Lunch': '',
-                                      'Dinner': ''
-                                    }),
+      body: Theme(
+        data: Theme.of(context),
+        child: Builder(
+          builder: (context) => TabBarView(
+            children: [
+              SafeArea(
+                child: Stack(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 10, 10, 5),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              DayRow(
+                                  day: days[0],
+                                  date: dateStampToday,
+                                  dataMap: dataMap[dateStampToday],
+                                  isToday: true),
+                              DayRow(
+                                  day: days[1],
+                                  date: dateStampToday
+                                      .subtract(Duration(days: 1)),
+                                  dataMap: dataMap[dateStampToday
+                                          .subtract(Duration(days: 1))] ??
+                                      {
+                                        'Breakfast': '',
+                                        'Lunch': '',
+                                        'Dinner': ''
+                                      }),
+                              DayRow(
+                                  day: days[2],
+                                  date: dateStampToday
+                                      .subtract(Duration(days: 2)),
+                                  dataMap: dataMap[dateStampToday
+                                          .subtract(Duration(days: 2))] ??
+                                      {
+                                        'Breakfast': '',
+                                        'Lunch': '',
+                                        'Dinner': ''
+                                      }),
+                              DayRow(
+                                  day: days[3],
+                                  date: dateStampToday
+                                      .subtract(Duration(days: 3)),
+                                  dataMap: dataMap[dateStampToday
+                                          .subtract(Duration(days: 3))] ??
+                                      {
+                                        'Breakfast': '',
+                                        'Lunch': '',
+                                        'Dinner': ''
+                                      }),
+                              DayRow(
+                                  day: days[4],
+                                  date: dateStampToday
+                                      .subtract(Duration(days: 4)),
+                                  dataMap: dataMap[dateStampToday
+                                          .subtract(Duration(days: 4))] ??
+                                      {
+                                        'Breakfast': '',
+                                        'Lunch': '',
+                                        'Dinner': ''
+                                      }),
+                              DayRow(
+                                  day: days[5],
+                                  date: dateStampToday
+                                      .subtract(Duration(days: 5)),
+                                  dataMap: dataMap[dateStampToday
+                                          .subtract(Duration(days: 5))] ??
+                                      {
+                                        'Breakfast': '',
+                                        'Lunch': '',
+                                        'Dinner': ''
+                                      }),
+                              DayRow(
+                                  day: days[6],
+                                  date: dateStampToday
+                                      .subtract(Duration(days: 6)),
+                                  dataMap: dataMap[dateStampToday
+                                          .subtract(Duration(days: 6))] ??
+                                      {
+                                        'Breakfast': '',
+                                        'Lunch': '',
+                                        'Dinner': ''
+                                      }),
+                            ],
+                          ),
+                        ), //Days Overview
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Container(
+                              height: 150.0,
+                              width: 100.0,
+                              child: CupertinoPicker(
+                                scrollController: FixedExtentScrollController(
+                                    initialItem: setInitialSelectedMeal()),
+                                itemExtent: 50.0,
+                                onSelectedItemChanged: (selectedIndex) {
+                                  _selectMeal(selectedIndex);
+                                },
+                                children: [
+                                  Center(
+                                    child: Text(
+                                      'Breakfast',
+                                      style:
+                                          TextStyle(fontFamily: 'JosefinSans'),
+                                    ),
+                                  ),
+                                  Center(
+                                    child: Text(
+                                      'Lunch',
+                                      style:
+                                          TextStyle(fontFamily: 'JosefinSans'),
+                                    ),
+                                  ),
+                                  Center(
+                                    child: Text(
+                                      'Dinner',
+                                      style:
+                                          TextStyle(fontFamily: 'JosefinSans'),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ), // Meal Picker
+                            Container(
+                              height: 150.0,
+                              width: 150.0,
+                              child: CupertinoPicker(
+                                scrollController:
+                                    FixedExtentScrollController(initialItem: 1),
+                                itemExtent: 80.0,
+                                onSelectedItemChanged: (selectedIndex) {
+                                  _selectFood(selectedIndex);
+                                },
+                                children: getFoodImages(20.0),
+                              ),
+                            ),
+//FoodPicker
                           ],
                         ),
-                      ), //Days Overview
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Container(
-                            height: 150.0,
-                            width: 100.0,
-                            child: CupertinoPicker(
-                              scrollController: FixedExtentScrollController(
-                                  initialItem: setInitialSelectedMeal()),
-                              itemExtent: 50.0,
-                              onSelectedItemChanged: (selectedIndex) {
-                                _selectMeal(selectedIndex);
-                              },
-                              children: [
-                                Center(
-                                  child: Text(
-                                    'Breakfast',
-                                    style: TextStyle(fontFamily: 'JosefinSans'),
-                                  ),
-                                ),
-                                Center(
-                                  child: Text(
-                                    'Lunch',
-                                    style: TextStyle(fontFamily: 'JosefinSans'),
-                                  ),
-                                ),
-                                Center(
-                                  child: Text(
-                                    'Dinner',
-                                    style: TextStyle(fontFamily: 'JosefinSans'),
-                                  ),
-                                ),
-                              ],
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            FlatButton(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50)),
+                              color: Colors.green[100],
+                              onPressed: () => _selectDate(context),
+                              child: Text(
+                                'Select Date: ${selectedDate.day}.${selectedDate.month}.',
+                                style: TextStyle(color: Colors.green[500]),
+                              ),
                             ),
-                          ), // Meal Picker
-                          Container(
-                            height: 150.0,
-                            width: 150.0,
-                            child: CupertinoPicker(
-                              scrollController:
-                                  FixedExtentScrollController(initialItem: 1),
-                              itemExtent: 80.0,
-                              onSelectedItemChanged: (selectedIndex) {
-                                _selectFood(selectedIndex);
-                              },
-                              children: getFoodImages(20.0),
-                            ),
-                          ),
-//FoodPicker
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          FlatButton(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50)),
-                            color: Colors.green[100],
-                            onPressed: () => _selectDate(context),
-                            child: Text(
-                              'Select Date: ${selectedDate.day}.${selectedDate.month}.',
-                              style: TextStyle(color: Colors.green[500]),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Align(
-                              alignment: Alignment.bottomRight,
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
                               child: Ink(
                                 decoration: const ShapeDecoration(
                                   color: Colors.lightGreen,
@@ -384,27 +387,27 @@ class _MyHomePageState extends State<MyHomePage> {
                                 ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Scrollbar(
-              child: PageView(
-                scrollDirection: Axis.vertical,
-                children: getRecordScreens(6, 0),
+              Scrollbar(
+                child: PageView(
+                  scrollDirection: Axis.vertical,
+                  children: getRecordScreens(6, 0),
+                ),
               ),
-            ),
-            Scrollbar(
-              child: PageView(
-                scrollDirection: Axis.vertical,
-                children: getRecordScreens(30, 0),
+              Scrollbar(
+                child: PageView(
+                  scrollDirection: Axis.vertical,
+                  children: getRecordScreens(30, 0),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
